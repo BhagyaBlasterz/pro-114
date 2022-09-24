22f5f6d06380e957e6ba52f86b93f8c4fe082931
Bhagya Zatkar <61934390+BhagyaBlasterz@users.noreply.github.com>
2022-09-24 16:05:19 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+a=33
+b=33
+if b > a:
+    print("B is greater than A")
+elif a == b:
+    print("A and B are Equal")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + "ing")
+
+my_function("Work")
+my_function("Run")
+my_function("Do")
